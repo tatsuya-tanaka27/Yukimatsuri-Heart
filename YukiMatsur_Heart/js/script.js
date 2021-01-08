ce399108a6528d97d29c6de3779fb0ef15013755
tatsuya-tanaka27 <75655016+tatsuya-tanaka27@users.noreply.github.com>
2021-01-08 13:27:13 +0900

    Add files via upload
    yukimatsuri test

---
+++ YukiMatsur_Heart/js/script.js
@@ -0,0 +1,6 @@
+document.getElementById("menuButton").addEventListener("click", function() {
+    this.classList.toggle("active");
+    document.getElementById("nav").classList.toggle("active");
+    document.getElementById("mask").classList.toggle("active");
+
+})
